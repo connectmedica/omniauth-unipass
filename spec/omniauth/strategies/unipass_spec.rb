c6fe8ad246fc614e99ea2cd182fb177a761a4d67
Karol Sarnacki <sodercober@gmail.com>
2012-02-23 11:32:20 +0100

    Add tests for authorize_params

--- spec/omniauth/strategies/unipass_spec.rb
+++ spec/omniauth/strategies/unipass_spec.rb
@@ -9,8 +9,8 @@ describe OmniAuth::Strategies::Unipass do
     @request.stub(:params){ {} }
     @request.stub(:cookies){ {} }
 
-    @client_id     = '123'
-    @client_secret = '53cr3tz'
+    @client_id     = '33ade5204037012f84783c075443e018'
+    @client_secret = '47380fc04037012f84783c075443e018'
   end
 
   subject do
@@ -52,6 +52,25 @@ describe OmniAuth::Strategies::Unipass do
       @request.stub(:url){ "#{url_base}/page/path" }
       subject.stub(:script_name){ '' } # to not depend from Rack env
       subject.callback_url.should eq("#{url_base}/auth/unipass/callback")
+    end
+  end
+
+  describe '#authorize_params' do
+    it 'includes default scope for email access' do
+      subject.authorize_params.should be_a(Hash)
+      subject.authorize_params[:scope].should eq('email')
+    end
+
+    it 'includes display parameter from request when present' do
+      @request.stub(:params){ {'display' => 'mobile'} }
+      subject.authorize_params.should be_a(Hash)
+      subject.authorize_params[:display].should eq('mobile')
+    end
+
+    it 'includes state parameter from request when present' do
+      @request.stub(:params){ {'state' => 'my_state'git} }
+      subject.authorize_params.should be_a(Hash)
+      subject.authorize_params[:state].should eq('my_state')
     end
   end
 
